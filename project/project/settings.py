b424493455bb17fc20f6a5d70be8af2275faff9c
SebastianTylicki <sebastian.tylicki@onet.pl>
2025-12-01 18:11:35 +0100

    Add accounts app and Custom User

--- project/project/settings.py
+++ project/project/settings.py
@@ -38,9 +38,19 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    
+    'rest_framework.authtoken',
+
     'recipes',
+    'accounts',
 ]
+
+AUTH_USER_MODEL = 'accounts.CustomUser'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
