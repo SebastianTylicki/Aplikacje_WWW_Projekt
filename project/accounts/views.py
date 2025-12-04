6a65a6b2d03d871eef16fb06d16de09019fe4f27
SebastianTylicki <sebastian.tylicki@onet.pl>
2025-12-04 17:35:58 +0100

    Add logout endpoint and fix registration

--- project/accounts/views.py
+++ project/accounts/views.py
@@ -1,20 +1,29 @@
 from rest_framework import status
 from rest_framework.response import Response
-from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import AllowAny
+from rest_framework.decorators import api_view, permission_classes, authentication_classes
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
 from rest_framework.authtoken.models import Token
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, get_user_model
 from .serializers import UserSerializer
+
+User = get_user_model()
 
 @api_view(['POST'])
 @permission_classes([AllowAny])
 def register_user(request):
+    username = request.data.get('username')
+    email = request.data.get('email')
+
+    if User.objects.filter(username=username).exists() or User.objects.filter(email=email).exists():
+        return Response({"message": "User with this data already exists"}, status=status.HTTP_400_BAD_REQUEST)
+
     serializer = UserSerializer(data=request.data)
     if serializer.is_valid():
         user = serializer.save()
         token, created = Token.objects.get_or_create(user=user)
         return Response({
-            'user_id': user.pk,
+            'user_id': user.pk, 
             'email': user.email,
             'token': token.key
         }, status=status.HTTP_201_CREATED)
@@ -33,3 +42,10 @@ def login_user(request):
         return Response({'token': token.key})
     else:
         return Response({'error': 'Invalid credentials'}, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['POST'])
+@authentication_classes([TokenAuthentication])
+@permission_classes([IsAuthenticated])
+def logout_user(request):
+    request.user.auth_token.delete()
+    return Response({"message": "Successfully logged out"}, status=status.HTTP_200_OK)
